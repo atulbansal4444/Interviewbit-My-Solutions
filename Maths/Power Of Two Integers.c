97156a7c8ce03ee8bc54c276c6d3cf59f7b91e8d
Atul Bansal <atulbansal4444@gmail.com>
2019-01-29 22:45:39 +0530

    Power of Two Integer added

---
+++ Maths/Power Of Two Integers.c
@@ -0,0 +1,17 @@
+int isPower(int A) {
+    if (A==1)  return 1;
+    int x;
+    for ( x=2; x<=sqrt(A); x++)
+    {
+        unsigned y = 2;
+        unsigned p = pow(x, y);
+        while (p<=A && p>0)
+        {
+            if (p==A)
+                return 1;
+            y++;
+            p = pow(x, y);
+         }
+    }
+    return 0;
+}
