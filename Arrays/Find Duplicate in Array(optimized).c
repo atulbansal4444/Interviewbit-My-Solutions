a1752f77a757f949528b500c5ebb212ba5fb9194
Atul Bansal <30015420+atulbansal4444@users.noreply.github.com>
2019-11-24 20:03:57 +0530

    Spacing.

--- Arrays/Find Duplicate in Array(optimized).c
+++ Arrays/Find Duplicate in Array(optimized).c
@@ -11,6 +11,7 @@ void swap(int* a, int* b)
     *a = *b;
     *b = t;
 }
+
 int partition (int arr[], int low, int high)
 {
     int pivot = arr[high];   
@@ -27,16 +28,17 @@ int partition (int arr[], int low, int high)
     swap(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
+
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-      
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
+
 int findRepeatingElement(int arr[],int n)
 {
     int i;
@@ -46,8 +48,8 @@ int findRepeatingElement(int arr[],int n)
     }
     return -1;
 }
+
 int repeatedNumber(const int* A, int n1) {
     quickSort(A,0,n1-1);
     return findRepeatingElement(A,n1);
-    
 }
